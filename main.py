4921cb6e070b9bb529d8d1dc7db89c2033880aa1
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-09-10 13:44:11 +0300

    Добавлена дата и карточки вин

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+env = Environment(
+    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+)
+
+template = env.get_template("template.html")
+
+current_year = datetime.datetime.now().year
+foundation_year = 1920
+years = current_year - foundation_year
+
+
+def get_year_word(years):
+    if years % 10 == 1 and years % 100 != 1:
+        return "год"
+    elif 2 <= years % 10 <= 4 and (years % 100 < 10 or years % 100 >= 20):
+        return "года"
+    else:
+        return "лет"
+    
+
+rendered_page = template.render(years=years, year_word=get_year_word(years))
+
+
+with open("index.html", "w", encoding="utf8") as file:
+    file.write(rendered_page)
+
+
+server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
