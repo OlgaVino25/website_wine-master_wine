4997168ec755f4526912593ec515786edc1e2e9e
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-09-11 10:04:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,50 +5,62 @@ import pandas
 from collections import defaultdict
 
 
-env = Environment(
-    loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
-)
+def load_wines_from_excel(file_path):
+    """Загружает данные о винах из Excel файла и группирует их по категориям"""
+    excel_data_df = pandas.read_excel(
+        io=file_path, sheet_name="Лист1", na_values="nan", keep_default_na=False
+    )
 
-template = env.get_template("template.html")
+    wines_list = excel_data_df.to_dict(orient="records")
 
-current_year = datetime.datetime.now().year
-foundation_year = 1920
-years = current_year - foundation_year
+    grouped_wines = defaultdict(list)
+    for wine in wines_list:
+        category = wine["Категория"]
+        grouped_wines[category].append(wine)
 
+    grouped_wines = dict(grouped_wines)
+    sorted_categories = sorted(grouped_wines.keys())
 
-def get_year_word(years):
-    if years % 10 == 1 and years % 100 != 1:
-        return "год"
-    elif 2 <= years % 10 <= 4 and (years % 100 < 10 or years % 100 >= 20):
-        return "года"
-    else:
-        return "лет"
-
-
-file_path = r"C:\DVMN\Layout\lesson_1\wine-master\website_wine-master_wine\wine3.xlsx"
-excel_data_df = pandas.read_excel(
-    io=file_path, sheet_name="Лист1", na_values="nan", keep_default_na=False
-)
-
-wines_list = excel_data_df.to_dict(orient="records")
-
-grouped_wines = defaultdict(list)
-for wine in wines_list:
-    category = wine['Категория']
-    grouped_wines[category].append(wine)
-
-grouped_wines = dict(grouped_wines)
-sorted_categories = sorted(grouped_wines.keys())
+    return sorted_categories, grouped_wines
 
 
-rendered_page = template.render(
-    years=years, year_word=get_year_word(years), sorted_categories=sorted_categories, grouped_wines=grouped_wines
-)
+def main():
+    env = Environment(
+        loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+    )
+
+    template = env.get_template("template.html")
+
+    current_year = datetime.datetime.now().year
+    foundation_year = 1920
+    years = current_year - foundation_year
+
+    def get_year_word(years):
+        if years % 10 == 1 and years % 100 != 1:
+            return "год"
+        elif 2 <= years % 10 <= 4 and (years % 100 < 10 or years % 100 >= 20):
+            return "года"
+        else:
+            return "лет"
+
+    file_path = (
+        r"C:\DVMN\Layout\lesson_1\wine-master\website_wine-master_wine\wine3.xlsx"
+    )
+    sorted_categories, grouped_wines = load_wines_from_excel(file_path)
+
+    rendered_page = template.render(
+        years=years,
+        year_word=get_year_word(years),
+        sorted_categories=sorted_categories,
+        grouped_wines=grouped_wines,
+    )
+
+    with open("index.html", "w", encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
 
 
-with open("index.html", "w", encoding="utf8") as file:
-    file.write(rendered_page)
-
-
-server = HTTPServer(("0.0.0.0", 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == "__main__":
+    main()
