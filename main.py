a22d74415b52846eb8ff1652536459899c32ef4d
OlgaVino25 <olgavinokyrova25@yandex.ru>
2025-09-15 10:28:29 +0300

    Структурирован скрипт

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 from collections import defaultdict
+
+
+def get_year_word(years):
+    if years % 10 == 1 and years % 100 != 11:
+        return "год"
+    elif 2 <= years % 10 <= 4 and (years % 100 < 10 or years % 100 >= 20):
+        return "года"
+    else:
+        return "лет"
 
 
 def load_wines_from_excel(file_path):
@@ -34,14 +43,6 @@ def main():
     current_year = datetime.datetime.now().year
     foundation_year = 1920
     years = current_year - foundation_year
-
-    def get_year_word(years):
-        if years % 10 == 1 and years % 100 != 1:
-            return "год"
-        elif 2 <= years % 10 <= 4 and (years % 100 < 10 or years % 100 >= 20):
-            return "года"
-        else:
-            return "лет"
 
     file_path = (
         r"C:\DVMN\Layout\lesson_1\wine-master\website_wine-master_wine\wine3.xlsx"
